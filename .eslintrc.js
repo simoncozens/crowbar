03100c16568182e8af86028e51479e87ecf2c7c0
Simon Cozens <simon@simon-cozens.org>
2020-06-26 11:48:56 +0100

    AirBNB eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,19 @@
 module.exports = {
-
-    "env": {
-        "browser": true,
-        "es2020": true
-    },
-    "extends": [
-        "react-app",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 11,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
-};
+  "extends": [
+    "react-app",
+    "airbnb",
+    "plugin:jsx-a11y/recommended",
+    "airbnb-typescript"
+  ],
+  "plugins": [
+    "jsx-a11y",
+  ],
+  "rules": {
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx", ".ts", ".tsx"] }],
+    "prettier/prettier": [
+      "error", {
+        "semi": false
+      }
+    ]
+  }
+}
