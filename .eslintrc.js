53ca2f1a673f0b02b89dbb36f54a38f1b3db5680
Simon Cozens <simon@simon-cozens.org>
2021-12-27 12:04:05 +0000

    This might be a terrible thing to do!

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,32 @@
 module.exports = {
-    extends: [
-        "react-app",
-        "plugin:jsx-a11y/recommended",
-        "airbnb-typescript",
-        "prettier",
-        "prettier/@typescript-eslint",
+  extends: [
+    "react-app",
+    "plugin:jsx-a11y/recommended",
+    "airbnb-typescript",
+    "prettier",
+    "prettier/@typescript-eslint",
+  ],
+  plugins: ["jsx-a11y", "prettier"],
+  rules: {
+    quotes: "off",
+    "import/no-extraneous-dependencies": [
+      "error",
+      {
+        devDependencies: false,
+        optionalDependencies: false,
+        peerDependencies: false,
+      },
     ],
-    plugins: ["jsx-a11y", "prettier"],
-    rules: {
-        quotes: "off",
-        "@typescript-eslint/quotes": ["error", "double"],
-        "react/jsx-filename-extension": [
-            1,
-            { extensions: [".js", ".jsx", ".ts", ".tsx"] },
-        ],
-        "@typescript-eslint/comma-dangle": "off",
-        "react/jsx-wrap-multilines": "off",
-        "prettier/prettier": "error",
-    },
-    parserOptions: {
-        project: "./tsconfig.json",
-    },
-}
+    "@typescript-eslint/quotes": ["error", "double"],
+    "react/jsx-filename-extension": [
+      1,
+      { extensions: [".js", ".jsx", ".ts", ".tsx"] },
+    ],
+    "@typescript-eslint/comma-dangle": "off",
+    "react/jsx-wrap-multilines": "off",
+    "prettier/prettier": "error",
+  },
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
+};
