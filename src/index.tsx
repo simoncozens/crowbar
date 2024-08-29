c712c1e7145270bf8164710c097a0483ecb7fc11
Simon Cozens <simon@simon-cozens.org>
2024-08-29 14:25:01 +0100

    Use latest version of harfbuzzjs

--- src/index.tsx
+++ src/index.tsx
@@ -4,28 +4,22 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
+import harfbuzz from "harfbuzzjs";
 import App from "./App";
 import appReducer from "./store/reducer";
-import hbjs from "./hbjs";
 
 declare let window: any;
 
 const store = createStore(appReducer, applyMiddleware(thunk));
-fetch(`${process.env.PUBLIC_URL}/harfbuzz.wasm`)
-  .then((response) => response.arrayBuffer())
-  .then((bytes) => WebAssembly.instantiate(bytes))
-  .then((results) => {
-    // @ts-ignore
-    const hb = hbjs(results.instance); // Dirty but works
-    window.harfbuzz = results.instance;
-    window.hbjs = hb;
+harfbuzz.then((hbjs: any) => {
+  window.hbjs = hbjs;
 
-    ReactDOM.render(
-      <React.StrictMode>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </React.StrictMode>,
-      document.getElementById("root")
-    );
-  });
+  ReactDOM.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+});
