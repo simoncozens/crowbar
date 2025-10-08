6cc131748467c6b28e26518eb6b4bfe0478669f5
Simon Cozens <simon@simon-cozens.org>
2025-10-08 15:37:41 +0000

    Fix light mode, see #82

--- src/App.tsx
+++ src/App.tsx
@@ -32,12 +32,6 @@ const theme = createTheme({
   colorSchemes: {
     dark: true,
   },
-  palette: {
-    mode: 'dark',
-    primary: {
-      main: '#ff5252',
-    },
-  },
 });
 
 const Offset = styled('div')(({ theme }) => theme.mixins.toolbar);
